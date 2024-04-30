54392ce75c62cf55f7be6b5fa6153a4960d64af1
Maciej <maciek_k112@wp.pl>
2024-04-30 18:28:17 +0200

    finished homescrean adding navbar and other futures

--- flutter_english_learning/lib/pages/home.dart
+++ flutter_english_learning/lib/pages/home.dart
@@ -9,12 +9,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // ignore: non_constant_identifier_names
   bool casual_boots=false, running_boots=false, high_heel_boots=false, tracking_boots=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 50.0, left:20.0, right: 20.0 ),
+        margin: EdgeInsets.only(top: 50.0, left:20.0 ),
         child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -26,6 +27,7 @@ class _HomeState extends State<Home> {
             
             ),
              Container(
+              margin: EdgeInsets.only(right: 20.0),
           padding: EdgeInsets.all(3) ,
           decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(10)),
           child: Icon(Icons.shopping_cart, color: Colors.white,),
@@ -39,16 +41,93 @@ class _HomeState extends State<Home> {
            Text("Discover and get great boots. ", 
             style:AppWidget.lightText()),
             SizedBox(height: 20.0,),
-           showItem(),
-            SizedBox(height: 20.0,),
-            Row(children: [
-              Container(
-                child: Column(children: [
-                  
-                ],),
-              )
-            ],
-
+           Container(
+            margin: EdgeInsets.only(right: 20.0),
+            child: showItem()),
+            SizedBox(height: 30.0,),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                Container(
+                  margin: EdgeInsets.all(5),
+                  child: Material(
+                    elevation: 5.90,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start, //text to left sight
+                        children: [
+                        Image.asset("images/casual_boots_1.png", width: 150, height: 150, fit: BoxFit.cover,),
+                        Text("Casual boots",
+                        style: AppWidget.semiboldedText(),),
+                        SizedBox(height: 5.0,),
+                         Text("Best for Summer",
+                         style: AppWidget.lightText(),),
+                        SizedBox(height: 5.0,),
+                         Text("\$25", style: AppWidget.semiboldedText(),)
+                      ],),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15.0,),
+                 Container(
+                  margin: EdgeInsets.all(5),
+                   child: Material(
+                    elevation: 5.90,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start, //text to left sight
+                        children: [
+                        Image.asset("images/casual_boots_2.png", width: 150, height: 150, fit: BoxFit.cover,),
+                        Text("Casual boots",
+                        style: AppWidget.semiboldedText(),),
+                        SizedBox(height: 5.0,),
+                         Text("Best for Summer",
+                         style: AppWidget.lightText(),),
+                        SizedBox(height: 5.0,),
+                         Text("\$25", style: AppWidget.semiboldedText(),)
+                      ],),
+                    ),
+                                   ),
+                 )
+                ]
+              
+              ),
+              
+            ),
+            SizedBox(height: 30.0,),
+            Container(
+              margin: EdgeInsets.only(right: 10.0),
+              child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Image.asset("images/Casual_boots_2.png", height: 150, width: 150,fit: BoxFit.cover,),
+                    SizedBox(width: 20.0,),
+                    Column(children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width/2,
+                        child: Text("Casual boots", style: AppWidget.semiboldedText(),)),
+                        SizedBox(height: 5.0,),
+                         Container(
+                        width: MediaQuery.of(context).size.width/2,
+                        child: Text("Best for Summer", style: AppWidget.lightText(),)),
+                        SizedBox(height: 5.0,),
+                         Container(
+                        width: MediaQuery.of(context).size.width/2,
+                        child: Text("\$55", style: AppWidget.semiboldedText(),))
+                    ],)
+                  ],),
+                ),
+              ),
             )
       ],),
       ),
